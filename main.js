2f395e9b595d6a95af96566d4371c0b58de95059
Colin Jaffe <balloonasaurus@gmail.com>
2019-10-01 15:19:58 -0400

    Adds tests for students.

--- main.js
+++ main.js
@@ -1,7 +1,48 @@
-function exampleFunction() {
-
+function isItLong(str) {
+  if (str.length > 20) {
+    return `That's a long string!`;
+  }
 }
 
+function isItMedium(str) {
+  if (str.length >= 10 && str.length <= 20) {
+    return `That's a regular sized string!`
+  }
+}
+
+function isItShort(str) {
+  if (str.length < 10) {
+    return `That's a small string!`
+  }
+}
+
+function howLongIsMyString(str) {
+  if (str.length > 20) {
+    return `That's a long string!`;
+  } else if (str.length >= 10) {
+    return `That's a regular sized string!`;
+  } else {
+    return `That's a small string!`;
+  }
+}
+
+function instructorHeight(instructor) {
+  if (instructor === 'Colin') {
+    return 62;
+  } else if (instructor === 'Mesuara') {
+    return 67;
+  } else {
+    return `I don't know that instructor!`;
+  }
+}
+
+
+
+
 module.exports = {
-  exampleFunction,
+  isItLong,
+  isItMedium,
+  isItShort,
+  howLongIsMyString,
+  instructorHeight,
 }
